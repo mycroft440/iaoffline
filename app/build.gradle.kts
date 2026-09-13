201bb7ca2437b61a318a84c0e5698c9548017d40
mycroft440 <alquimistadodetalhe@gmail.com>
2026-09-13 05:26:44 -0300

    fix: declare Android API 37.0 compile SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,11 @@ plugins {
 
 android {
     namespace = "com.example.ialocal"
-    compileSdk = 37
+    compileSdk {
+        version = release(37) {
+            minorApiLevel = 0
+        }
+    }
 
     defaultConfig {
         applicationId = "com.example.ialocal"
